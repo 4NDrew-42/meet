8c15da6b5a88fdee28916bb192a851ab8c9fbd59
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2024-03-18 19:35:36 -0500

    feat: dependencies, function:getAuthUrl

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,29 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { google } = require('googleapis');
+const calendar = google.calendar('v3');
+const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = ['https://4NDrew-42.github.io/meet/'];
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, redirect_uris[0]);
+
+module.exports.getAuthURL = async () => {
+	//Scopes array is passed to the `scope` option.
+
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true,
+		},
+		body: JSON.stringify({
+			authUrl,
+		}),
+	};
 };
