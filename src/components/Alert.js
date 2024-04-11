cdd541773ba641f40673b7fa27a95955f3fd5926
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2024-04-11 17:45:08 -0500

    feat: Warning for Offline User

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,4 +48,12 @@ class ErrorAlert extends Alert {
 	}
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+	constructor(props) {
+		super(props);
+		this.color = 'rgb(255, 165, 0)'; // orange
+		this.bgColor = 'rgb(255, 235, 220)'; // light orange
+	}
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
